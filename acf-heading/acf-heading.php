4daec22df5d50fdc6e73b7fac7aa0383db017194
Ivan Mattie <mattieivan@gmail.com>
2018-01-17 11:27:30 -0700

    Basic cleanup and setup based on template

---
+++ acf-heading/acf-heading.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: Heading
+Plugin URI: https://seo.com
+Description: Adds ACF field that lets you select the heading level.
+Version: 1.0.0
+Author: Ivan Mattie
+Author URI: https://www.ivanmattie.com/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) exit;
+
+
+// check if class already exists
+if( !class_exists('simple_acf_plugin_heading') ) :
+
+class simple_acf_plugin_heading {
+
+	// vars
+	var $settings;
+
+
+	/*
+	*  __construct
+	*
+	*  This function will setup the class functionality
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	function __construct() {
+
+		// settings
+		// - these will be passed into the field class.
+		$this->settings = array(
+			'version'	=> '1.0.0',
+			'url'		=> plugin_dir_url( __FILE__ ),
+			'path'		=> plugin_dir_path( __FILE__ )
+		);
+
+
+		// set text domain
+		// https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
+		load_plugin_textdomain( 'acf-heading', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' );
+
+
+		// include field
+		add_action('acf/include_field_types', 	array($this, 'include_field_types')); // v5
+		add_action('acf/register_fields', 		array($this, 'include_field_types')); // v4
+
+	}
+
+
+	/*
+	*  include_field_types
+	*
+	*  This function will include the field type class
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	$version (int) major ACF version. Defaults to false
+	*  @return	n/a
+	*/
+
+	function include_field_types( $version = false ) {
+
+		// support empty $version
+		if( !$version ) $version = 4;
+
+
+		// include
+		include_once('fields/class-simple-acf-field-heading-v' . $version . '.php');
+
+	}
+
+}
+
+
+// initialize
+new simple_acf_plugin_heading();
+
+
+// class_exists check
+endif;
+
+?>
